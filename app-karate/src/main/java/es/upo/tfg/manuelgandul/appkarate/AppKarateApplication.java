0ab4c199e7c8f561e867394c7a231f178f3726b0
Manuel Gandul Pérez <manu.gandul@gmail.com>
2019-11-30 15:13:33 +0100

    Creating the project

---
+++ app-karate/src/main/java/es/upo/tfg/manuelgandul/appkarate/AppKarateApplication.java
@@ -0,0 +1,13 @@
+package es.upo.tfg.manuelgandul.appkarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKarateApplication.class, args);
+	}
+
+}
